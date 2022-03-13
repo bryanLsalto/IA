ebb5a091c2b7e28aa3533f4a5034b02e0b60c562
bryanLsalto <55315117+bryanLsalto@users.noreply.github.com>
2022-03-13 04:00:27 +0100

    Add files via upload
    pràcticas hechas

---
+++ Practica 1/src/RedSensGoalTest.java
@@ -0,0 +1,10 @@
+import aima.search.framework.GoalTest;
+
+public class RedSensGoalTest implements GoalTest {
+
+    public boolean isGoalState(Object state){
+
+        return((RedSens) state).isGoal();
+        //equivalente a return false
+    }
+}
